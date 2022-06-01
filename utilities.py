70de30dfd0ad5b00cf8400e5cfa54f06d858df52
dgilford <dgilford@climatecentral.org>
2022-06-01 21:04:49 +0000

    removed unnecessary fx

--- utilities.py
+++ utilities.py
@@ -141,32 +141,3 @@ def KtoC(data_in_kelvin):
 # define a function to convert longitudes from 180W-180E to 0-360E
 def lon180to360(lon180):
     return(lon180 % 360)
-
-
-
-# Function to calculate the regression between a global xarray and a timeseries
-def global_xr_regression(yx,ts,missnum=-999):
-    
-    # regress the global annual DataArray, yx(t), against the timeseries, ts
-    # note that y should lead x (lat before lon)
-    
-    # create the output arrays
-    slope_yx = xr.full_like(yx.groupby('time.month').mean().squeeze().copy(deep='True'),np.nan,dtype='float')
-    stderr_yx=slope_yx.copy(deep=True)
-    
-    
-    # find the valid locations
-    validt=yx.where(yx>missnum)
-    
-    # loop over latitudes and longitude
-    for xi in range(len(yx.lon)):
-        for yi in range(len(yx.lat)):
-            # find the locations where we have valid years
-            # if at least one, calculated the regression and parameters
-            validi=~np.isnan(validt[:,yi,xi])
-            if np.sum(validi)>0:
-                slope_yx[yi,xi],_,_,_,stderr_yx[yi,xi] = linregress(ts[validi.drop('time')],validt[validi.drop('time'),yi,xi])
-            del validi
-    
-    # go back to the above program level
-    return(slope_yx,stderr_yx)
